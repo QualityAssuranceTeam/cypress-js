4e16752d86bd7800e888450df773107630029f00
yahmedov <>
2020-02-25 01:39:54 +0200

    login test

--- cypress/integration/tests/test.spec.js
+++ cypress/integration/tests/test.spec.js
@@ -1,8 +1,15 @@
-describe('My First Test', function () {
-  it('Sign In', function () {
+describe('My First Test', () => {
+  it('Sign In', () => {
     cy.visit('/ghost/#/signin')
+
+    cy.fixture('users/admin').then(adminUser => {
+      cy.get('.email').type(adminUser.email)
+      cy.get('.password').type(adminUser.password)
+    })
+    cy.get('.login').click()
+    // TODO: Assert new URL
   })
-  before('setup Admin User', function () {
+  before('setup Admin User', () => {
     cy.fixture('users/admin').then(adminUser => {
       cy.request({
         method: 'POST',
